dc7d33612055f20526b621cbee6398a408db08f3
nikolovGeorgi <gnikolov@sfu.ca>
2017-04-17 20:23:04 +0000

    fixed missing letter_opener gem error

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,13 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
+
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+# require "letter_opener" if Rails.env.development?
 
 module Jungle
   class Application < Rails::Application
